319eb85ee2170565b8a0ee742d183c6682140fd3
eerokkk <eerok228@gmail.com>
2023-01-19 11:41:35 +0500

    2 commit
    Ended create 1 level of game

--- Assets/_Project/Scripts/Game/Game.cs
+++ Assets/_Project/Scripts/Game/Game.cs
@@ -5,20 +5,48 @@ public class Game : MonoBehaviour
 {
     public static Game Instance;
 
+    public enum GameState
+    {
+        Lose,
+        Playing,
+        Win
+    }
+
+    public GameState CurrentGameState { get; private set; }
+
     [SerializeField]
     private Player player;
+    [SerializeField]
+    private GameObject losePanel;
+    [SerializeField]
+    private GameObject winPanel;
 
     private void Awake()
     {
         Instance = GetComponent<Game>();
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        CurrentGameState = GameState.Playing;
     }
 
     private void Update()
     {
         if (player.GetPlayerHealthPoints() == 0)
         {
-            print("Game Over!");
+            player.gameObject.SetActive(false);
+            OnPlayerLose();
         }
+    }
+
+    public void OnPlayerWin()
+    {
+        CurrentGameState = GameState.Win;
+        player.gameObject.SetActive(false);
+        winPanel.SetActive(true);
+    }
+
+    private void OnPlayerLose()
+    {
+        CurrentGameState = GameState.Lose;
+        losePanel.SetActive(true);
     }
 }
